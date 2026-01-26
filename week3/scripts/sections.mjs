eb7732e660d240dc880605666eb94d3aef943df6
emerald067 <eawoke@byupathway.edu>
2026-01-26 12:55:42 +0100

    Javascript modules excercise

---
+++ week3/scripts/sections.mjs
@@ -0,0 +1,9 @@
+export function setSectionSelection(byuiCourse) {
+  const sectionSelect = document.querySelector("#sectionNumber");
+  sections.forEach((section) => {
+    const option = document.createElement("option");
+    option.value = section.sectionNumber;
+    option.textContent = `${section.sectionNumber}`;
+    sectionSelect.appendChild(option);
+  });
+}
